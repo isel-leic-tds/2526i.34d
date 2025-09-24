2314da99cf474c1404de7fffd00f40f163df804b
Afonso Remédios <afonso.remedios@isel.pt>
2025-09-24 20:00:29 +0100

    Add isLeapYear, add isLastDayOfMont and start implementing addDays

--- _2_Date/src/main/kotlin/isel/tds/Date.kt
+++ _2_Date/src/main/kotlin/isel/tds/Date.kt
@@ -19,10 +19,35 @@ package isel.tds;
 //    constructor(y: Int): this(y, 1, 1)
 //}
 
+//class Date (val year: Int, val month: Int = 1, val day: Int = 1){
+//    val isLeapYear: Boolean
+//        get() = year%4 == 0 && year%100!=0 || year%400==0
+//
+//    init{
+//        require( year>0){"Invalid year"}
+//        require(month>0) {"Invalid month"}
+//        require(day>0) { "Invalid day"}
+//
+//        check(year>0){"Invalid year"}
+//    }
+//}
 class Date (val year: Int, val month: Int = 1, val day: Int = 1){
     init{
         require( year>0){"Invalid year"}
-        require(month>0) {"Invalid month"}
-        require(day>0) { "Invalid day"}
+        require(month>0 && month <=12) {"Invalid month"}
+        require(day>0 && day<=this.lastDayOfMonth) { "Invalid day"}
     }
 }
+val Date.isLeapYear: Boolean
+    get() = year%4 == 0 && year%100!=0 || year%400==0
+
+private val daysOfMonth = listOf(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+
+private val Date.lastDayOfMonth: Int
+    get() = if(month==2 && isLeapYear) 29 else daysOfMonth[month-1]
+val Date.isLastDayOfMonth: Boolean
+    get() = day == this.lastDayOfMonth
+
+fun Date.addDays(daysToAdd: Int): Date = Date(1,1,1)
+
+operator fun Date.plus(daysToAdd: Int): Date = Date(1,1,1)
--- _2_Date/src/test/kotlin/isel/tds/DateTest.kt
+++ _2_Date/src/test/kotlin/isel/tds/DateTest.kt
@@ -3,6 +3,8 @@ package isel.tds;
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class DateTest {
 
@@ -42,4 +44,60 @@ class DateTest {
         assertFailsWith<IllegalArgumentException>( block={Date(2025, 11, 0)})
     }
 
+    @Test
+    fun `test date is Leap Year`(){
+        val date = Date(2024, 11, 30)
+        assertTrue(date.isLeapYear)
+
+        val dateLeapYear400Multiple = Date(2400, 11, 30)
+        assertTrue(dateLeapYear400Multiple.isLeapYear)
+    }
+    @Test
+    fun `test date isn't Leap Year`(){
+        val date = Date(2025, 11, 30)
+        assertFalse(date.isLeapYear)
+
+        val dateNotLeapYear100Multiple = Date(2100, 11, 30)
+        assertFalse(dateNotLeapYear100Multiple.isLeapYear)
+
+        val dateLeapYear400MultiplePlusOne = Date(2401, 11, 30)
+        assertFalse(dateLeapYear400MultiplePlusOne.isLeapYear)
+    }
+
+    @Test
+    fun `test last day of month expecting true`(){
+        val dateLastDayOfJan = Date(2025, 1, 31)
+        assertTrue( dateLastDayOfJan.isLastDayOfMonth)
+        val dateLastDayOfFebLeapYear = Date(2025, 2, 28)
+        assertTrue( dateLastDayOfFebLeapYear.isLastDayOfMonth)
+        val dateLastDayOfFebNotLeapYear = Date(2024, 2, 29)
+        assertTrue( dateLastDayOfFebNotLeapYear.isLastDayOfMonth)
+        val dateLastDayOfDecember = Date(2024, 12, 31)
+        assertTrue( dateLastDayOfDecember.isLastDayOfMonth)
+    }
+
+    @Test
+    fun `test last day of month expecting false`(){
+        val dateFirstDayOfJan = Date(2025, 1, 1)
+        assertFalse( dateFirstDayOfJan.isLastDayOfMonth)
+        val dateLastDayOfJan = Date(2025, 1, 30)
+        assertFalse( dateLastDayOfJan.isLastDayOfMonth)
+        val dateLastDayOfFebLeapYear = Date(2025, 2, 27)
+        assertFalse( dateLastDayOfFebLeapYear.isLastDayOfMonth)
+        val dateLastDayOfFebNotLeapYear = Date(2024, 2, 28)
+        assertFalse( dateLastDayOfFebNotLeapYear.isLastDayOfMonth)
+        val dateLastDayOfDecember = Date(2024, 12, 30)
+        assertFalse( dateLastDayOfDecember.isLastDayOfMonth)
+    }
+
+    @Test
+    fun `test add days`(){
+        val dateFromAddDays = Date(2024, 2, 1).addDays(5)
+        assertEquals( 6, dateFromAddDays.day)
+
+        val dateFromPlusOperator = Date(2024, 2, 1) + 5
+        assertEquals( 6, dateFromPlusOperator.day)
+    }
 }
+
+
